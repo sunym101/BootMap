133f8299f76e2304599198392f72b0321e29e025
sunyongmeng <sunyongmeng@LAPTOP-1C2EREA1>
2019-01-15 11:44:26 +0800

    1. BootMap-0.0.1.bat修改，追加外置配置文件读取。

--- BootMap-0.0.1.bat
+++ BootMap-0.0.1.bat
@@ -1,5 +1,3 @@
-rem java -jar ./target/BootMap-0.0.1.jar
+java -jar ./target/BootMap-0.0.1.jar
 
-java –jar -Dspring.config.location=D:/application.properties ./target/BootMap-0.0.1.jar
-
-pause
+rem java –jar -Dspring.config.location=D:\application.properties .\target\BootMap-0.0.1.jar
